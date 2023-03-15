30a26bbbeba57b8aefbcd4d5a6cf98dc9913d611
Besufekad Alemu <besurye@gmail.com>
2023-03-15 17:41:49 +0300

    first draft of pop-up,comments and likes

---
+++ src/Modules/Methods-all.js
@@ -0,0 +1,209 @@
+import { getData, sendData } from './Api-call.js';
+import FoodList from './display.js';
+
+const foodList = new FoodList();
+
+const FOOD_API_BASE_URL = 'https://www.themealdb.com/api/json/v1/1/';
+const ALL_FOOD_ENDPOINT = 'filter.php?a=Italian';
+const ALL_FOOD_API_URL = FOOD_API_BASE_URL + ALL_FOOD_ENDPOINT;
+const INV_API_BASE = 'https://us-central1-involvement-api.cloudfunctions.net/'
+  + 'capstoneApi/apps/';
+const INV_API_KEY = 'zX9lc5HNiZeTfJrwouGw';
+const LIKES_ENDPOINT = '/likes';
+const COMMENT_ENDPOINT = '/comments';
+const foodListWrapper = document.getElementById('all-foods');
+const commentPopup = document.getElementById('comment-popup');
+const itemCounter = document.getElementById('count-foods');
+
+export const getComments = (id) => new Promise((resolve) => {
+  const parameter = `?item_id=${id}`;
+  const COMMENT_API = INV_API_BASE + INV_API_KEY + COMMENT_ENDPOINT + parameter;
+  getData(COMMENT_API).then((commentsFromAPI) => {
+    if (commentsFromAPI.error) commentsFromAPI = [];
+    const validComments = commentsFromAPI.filter((theComment) => {
+      const username = theComment.username.trim();
+      const comment = theComment.comment.trim();
+      return username.length && comment.length;
+    });
+    foodList.addComments(id, validComments.reverse());
+    resolve();
+  });
+});
+
+// const humanReadableDate = (strDate) => {
+//   const diff = Date.now() - Date.parse(strDate);
+//   const diffInDays = Math.floor(diff / (1000 * 60 * 60 * 24));
+//   if (diffInDays === 0) return 'Today';
+//   if (diffInDays === 1) return 'Yesterday';
+//   return `${diffInDays} days ago`;
+// };
+
+// const postComment = (id, input, textarea) => {
+//   const ADD_COMMENT_URL = INV_API_BASE + INV_API_KEY + COMMENT_ENDPOINT;
+//   const data = {
+//     item_id: id,
+//     username: input.value,
+//     comment: textarea.value,
+//   };
+
+//   sendData(ADD_COMMENT_URL, data).then((res) => {
+//     if (res.status === 201) {
+//       input.value = '';
+//       textarea.value = '';
+//       const commentWrapper = document.getElementById('comments');
+//       commentWrapper.innerHTML += `<li class="comment">
+//         <h4 class="comment-author">${data.username}</h4>
+//         <p class="comment-message">${data.comment}</p>
+//         </li> `;
+//     }
+//   });
+// };
+
+export const displayPopUp = (id) => {
+  commentPopup.classList.add('show');
+  commentPopup.innerHTML = `      <div id="popup-btn-close">&times;</div>
+    <div class="comment-popup-wrapper">
+      <div class="info-section">
+        <img id="food-image" src="${foodList.foods[id].image}" alt="" />
+        <h2 id="name-food">${foodList.foods[id].title}</h2>
+        <p id="recipes">...</p>
+      </div>
+      <div class="comments-section">
+        <h3>Add Comment:</h3>
+        <form id="comment-form">
+          <input type="text" name="name" placeholder="Your Name" required />
+          <textarea
+            name="comment"
+            rows="5"
+            placeholder="Your Comment"
+            required
+          ></textarea>
+          <button id="button" type="submit">Submit</button>
+        </form>
+
+        <h3 id="comments-header">Comments:</h3>
+        <ul id="comments">
+        </ul>
+      </div>
+    </div>`;
+
+  const commentForm = document.getElementById('comment-form');
+  commentForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    // postComment(id, e.target.name, e.target.comment);
+  });
+
+  const closeButton = document.getElementById('popup-btn-close');
+  closeButton.addEventListener('click', () => {
+    commentPopup.classList.remove('show');
+  });
+  const URL = `${FOOD_API_BASE_URL}lookup.php?i=${id}`;
+  getData(URL).then((res) => {
+    const foodItem = res.meals[0];
+    const foodDescElement = document.getElementById('recipes');
+    foodDescElement.innerHTML = foodItem.strInstructions;
+  });
+  getComments(id).then(() => {
+    const { comments } = foodList.foods[id];
+    const commentsHeader = document.getElementById('comments-header');
+    const commentWrapper = document.getElementById('comments');
+    if (comments.length) {
+      // add counter to comments header
+      commentsHeader.innerHTML += `<span class="food-count-icon">${foodList.getCommentsCount(
+        id,
+      )}</span>`;
+
+//       comments.forEach((comment) => {
+//         commentWrapper.innerHTML += `<li class="comment">
+//         <div class="comment-header">
+//           <h4 class="comment-author">${comment.username}</h4>
+//           <span class="comment-date">${humanReadableDate(
+//     comment.creation_date,
+//   )}</span>
+//         </div>
+//         <p class="comment-message">${comment.comment}</p>
+//         </li> `;
+//       });
+    } else {
+      commentWrapper.innerHTML = 'no comments';
+    }
+  });
+};
+
+export const likeFood = (id) => {
+  const url = INV_API_BASE + INV_API_KEY + LIKES_ENDPOINT;
+  const data = {
+    item_id: id,
+  };
+
+  // update like counter
+  sendData(url, data).then((res) => {
+    if (res.status === 201) {
+      const newLikes = foodList.getLikes(id) + 1;
+      foodList.setLikes(id, newLikes);
+      const likeWrapper = document.getElementById(id);
+      const counterElement = likeWrapper.querySelector('.count-likes');
+      counterElement.innerHTML = foodList.getLikesText(id);
+    }
+  });
+};
+
+export const showAllFood = () => {
+  // clear loading text
+  foodListWrapper.innerHTML = '';
+  Object.keys(foodList.foods).forEach((foodId) => {
+    const food = foodList.foods[foodId];
+    foodListWrapper.innerHTML += `
+    <div class="container-food" id="${foodId}">
+      <img src="${food.image}" alt="image of a meal" />
+      <div class="name-like">
+        <h3 class="name-food">${food.title}</h3>
+        <div class="give-likes">
+          <i class="fa fa-heart" aria-hidden="true"></i>
+          <div class="count-likes">
+            ${foodList.getLikesText(foodId)}
+          </div>
+        </div>
+      </div>
+      <button class="btn btn-comment">Click for More</button>
+    </div>
+    `;
+  });
+
+  const commentsButtons = foodListWrapper.querySelectorAll('.btn-comment');
+  commentsButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      displayPopUp(button.parentElement.id);
+    });
+  });
+
+  const likeButtons = foodListWrapper.querySelectorAll('.give-likes');
+  likeButtons.forEach((likeButton) => {
+    const foodId = likeButton.parentElement.parentElement.id;
+    likeButton.addEventListener('click', () => {
+      likeFood(foodId);
+    });
+  });
+};
+
+export const getAllFoodData = () => new Promise((resolve) => {
+  getData(ALL_FOOD_API_URL).then((res) => {
+    foodList.addFoods(res.meals);
+    resolve();
+  });
+});
+
+export const getAllLikes = () => new Promise((resolve) => {
+  const ALL_LIKES_API_URL = INV_API_BASE + INV_API_KEY + LIKES_ENDPOINT;
+  getData(ALL_LIKES_API_URL).then((likesFromAPI) => {
+    likesFromAPI.forEach((likeObject) => {
+      foodList.setLikes(likeObject.item_id, likeObject.likes);
+    });
+    resolve();
+  });
+});
+
+export const displayitemCounter = () => {
+  const size = foodList.getItemsCount();
+  itemCounter.innerHTML = `<span class="food-count-icon">${size}<span>`;
+};
--- src/Modules/display.js
+++ src/Modules/display.js
@@ -1,32 +1,86 @@
-export default class MealList {
-   getData = () => {
-     fetch('https://www.themealdb.com/api/json/v1/1/filter.php?a=Italian')
-       .then((response) => response.json())
-       .then((data) => {
-         console.log(data);
-         const { meals } = data;
-         const allFoods = document.querySelector('.all-foods');
-         meals.forEach((meal) => {
-           const mealDiv = `
-            <div class="meal">
-              <h3>${meal.strMeal}</h3>
-              <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
-              <div class="button-container">
-                <button class="likes-btn">Likes <span class="likes-count">0</span></button>
-                <button>Comments</button>
-              </div>
-            </div>
-          `;
-           allFoods.insertAdjacentHTML('beforeend', mealDiv);
+export default class FoodList {
+  constructor() {
+    this.foods = {};
+  }
 
-           const likesBtn = document.querySelector('.likes-btn');
-           const likesCount = document.querySelector('.likes-count');
-           let numLikes = 0;
-           likesBtn.addEventListener('click', () => {
-             numLikes++;
-             likesCount.textContent = numLikes;
-           });
-         });
-       });
-   };
+  addFood(id, title, image) {
+    this.foods[id] = {
+      title,
+      image,
+      comments: [],
+    };
+  }
+
+  addFoods(allFoods) {
+    allFoods.forEach((food) => {
+      this.addFood(food.idMeal, food.strMeal, food.strMealThumb);
+    });
+  }
+
+  setLikes(id, likes) {
+    if (Object.keys(this.foods).includes(id)) {
+      this.foods[id].likes = likes;
+    }
+  }
+
+  getLikes(id) {
+    return this.foods[id].likes || 0;
+  }
+
+  getLikesText(id) {
+    const likes = this.getLikes(id);
+    if (likes <= 1) {
+      return `${likes} like`;
+    }
+    return `${likes} likes`;
+  }
+
+  addComments(id, comments) {
+    this.foods[id].comments = comments;
+  }
+
+  getComments(id) {
+    return this.foods[id].comments;
+  }
+
+  getCommentsCount(id) {
+    return this.foods[id].comments.length;
+  }
+
+  getItemsCount() {
+    return Object.keys(this.foods).length;
+  }
 }
+
+// export default class MealList {
+//    getData = () => {
+//      fetch('https://www.themealdb.com/api/json/v1/1/filter.php?a=Italian')
+//        .then((response) => response.json())
+//        .then((data) => {
+//          console.log(data);
+//          const { meals } = data;
+//          const allFoods = document.querySelector('.all-foods');
+//          meals.forEach((meal) => {
+//            const mealDiv = `
+//             <div class="meal">
+//               <h3>${meal.strMeal}</h3>
+//               <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+//               <div class="button-container">
+//                 <button class="likes-btn">Likes <span class="likes-count">0</span></button>
+//                 <button>Comments</button>
+//               </div>
+//             </div>
+//           `;
+//            allFoods.insertAdjacentHTML('beforeend', mealDiv);
+
+//            const likesBtn = document.querySelector('.likes-btn');
+//            const likesCount = document.querySelector('.likes-count');
+//            let numLikes = 0;
+//            likesBtn.addEventListener('click', () => {
+//              numLikes++;
+//              likesCount.textContent = numLikes;
+//            });
+//          });
+//        });
+//    };
+// }
