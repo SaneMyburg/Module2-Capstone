60356df0c30eb00be7dd3ca06e8fbbd2caaf404e
SaneMyburg <sane.myburg@gmail.com>
2023-03-15 09:28:01 +0200

    Add number of likes in getData method

--- src/Modules/display.js
+++ src/Modules/display.js
@@ -1,23 +1,32 @@
 export default class MealList {
    getData = () => {
-     fetch('https://themealdb.com/api/json/v1/1/search.php?s')
-       .then((response) => response.json())
-       .then((data) => {
-         const { meals } = data;
-         const allFoods = document.querySelector('.all-foods');
-         meals.forEach((meal) => {
-           const mealDiv = `
+    fetch('https://themealdb.com/api/json/v1/1/search.php?s')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        const { meals } = data;
+        const allFoods = document.querySelector('.all-foods');
+        meals.forEach((meal) => {
+          const mealDiv = `
             <div class="meal">
               <h3>${meal.strMeal}</h3>
               <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
               <div class="button-container">
-                <button>Likes</button>
+                <button class="likes-btn">Likes <span class="likes-count">0</span></button>
                 <button>Comments</button>
               </div>
             </div>
           `;
-           allFoods.insertAdjacentHTML('beforeend', mealDiv);
-         });
-       });
-   };
+          allFoods.insertAdjacentHTML('beforeend', mealDiv);
+
+          const likesBtn = document.querySelector('.likes-btn');
+          const likesCount = document.querySelector('.likes-count');
+          let numLikes = 0;
+          likesBtn.addEventListener('click', () => {
+            numLikes++;
+            likesCount.textContent = numLikes;
+          });
+        });
+      })
+  };  
 }
