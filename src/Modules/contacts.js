430dd6c59907d754e23a15c42bce51d84fa4f63f
SaneMyburg <sane.myburg@gmail.com>
2023-03-18 13:02:03 +0200

    Add styling in contacts.js

--- src/Modules/contacts.js
+++ src/Modules/contacts.js
@@ -4,33 +4,34 @@ const contactsPopUp = () => {
   contactUs.className = 'contacts';
   contactUs.innerHTML = `      
       <div id="popup-btn-close">&times;</div>
-      <h2>Contacts:</h2>
-      <div class"contact-details>
+        <div class="contact-h2">
+          <h2>Contacts</h2>
+        </div>
+      <div class="contact-details">
         <div>
          <p>
           👤 Sane Myburg <br>
-          <a href="https://github.com/SaneMyburg">GitHub</a>
-          <a href="https://twitter.com/@SaneMyburg">Twitter</a>
-          <a href="https://linkedin.com/in/SaneMyburg">LinkedIn</a>
+          <a href="https://github.com/SaneMyburg">GitHub</a><br>
+          <a href="https://twitter.com/@SaneMyburg">Twitter</a><br>
+          <a href="https://linkedin.com/in/SaneMyburg">LinkedIn</a><br>
          </p>
         </div>
         <div>
          <p>
           👤 Besufekad Alem <br>
-          <a href="https://github.com/Besufekad-HAZ">GitHub</a>
-          <a href="https://twitter.com/BesufekadAlemu7">Twitter</a>
-          <a href="www.linkedin.com/in/besura">LinkedIn</a>
+          <a href="https://github.com/Besufekad-HAZ">GitHub</a><br>
+          <a href="https://twitter.com/BesufekadAlemu7">Twitter</a><br>
+          <a href="www.linkedin.com/in/besura">LinkedIn</a><br>
          </p>
         </div>
-      </div> `
+      </div> `;
 
-      popUp.appendChild(contactUs);
+  popUp.appendChild(contactUs);
 
-  const close = document.querySelector('#popup-btn-close');   
+  const close = document.querySelector('#popup-btn-close');
   close.addEventListener('click', () => {
     contactUs.remove();
   });
-   
-}
+};
 
 export default contactsPopUp;
